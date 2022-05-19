0ebc463813c72425deabdde049e789a26595ca2a
Tigran Mekinyan <tmekinyan@mail.ru>
2022-05-19 20:04:21 +0200

    Add Laravel binding example

--- app/Infrastructure/Laravel/Providers/AppServiceProvider.php
+++ app/Infrastructure/Laravel/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        //$this->app->bind(Interface::class, Implementation::class);
     }
 
     /**
