63733341b060275b65937c6d44d0653134d6b8e2
Tigran Mekinyan <tmekinyan@mail.ru>
2023-01-04 17:44:38 +0100

    Add command and query bus interfaces

---
+++ app/Domain/Shared/Bus/Command.php
@@ -0,0 +1,5 @@
+<?php namespace App\Domain\Shared\Bus;
+
+interface Command
+{
+}
--- app/Infrastructure/Laravel/Controller.php
+++ app/Infrastructure/Laravel/Controller.php
@@ -2,12 +2,40 @@
 
 namespace App\Infrastructure\Laravel;
 
+use App\Domain\Shared\Bus\Command;
+use App\Domain\Shared\Bus\Query;
+use App\Domain\Shared\Exception\DomainException;
+use Illuminate\Events\Dispatcher;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, ValidatesRequests;
+
+    private Dispatcher $bus;
+
+    public function __construct(Dispatcher $dispatcher)
+    {
+        $this->bus = $dispatcher;
+    }
+
+    /**
+     * @throws DomainException
+     */
+    protected function dispatch(Command $command): void
+    {
+        $this->bus->dispatch($command);
+    }
+
+    /**
+     * @throws DomainException
+     */
+    protected function ask(Query $query)
+    {
+        $response = $this->bus->dispatch($query);
+
+        return $response[0] ?? null;
+    }
 }
